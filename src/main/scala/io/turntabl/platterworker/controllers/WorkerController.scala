758955890ec10f18d0bc9181dc16b02bcdf50de6
idawud <ismaildawud96@gmail.com>
2019-12-12 14:22:27 +0000

    schedule route ready

--- src/main/scala/io/turntabl/platterworker/controllers/WorkerController.scala
+++ src/main/scala/io/turntabl/platterworker/controllers/WorkerController.scala
@@ -3,10 +3,9 @@ package io.turntabl.platterworker.controllers
 import java.nio.file.{Files, Paths}
 import java.time.LocalDateTime
 
-import com.google.gson.JsonObject
+import com.google.gson.JsonParser
 import io.turntabl.platterworker.AWS.CloudStorage
 import io.turntabl.platterworker.services.WeatherDataProcessing
-import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.{GetMapping, RestController}
 
 
@@ -18,17 +17,16 @@ class WorkerController {
     "Welcome Home"
   }
 
-  @GetMapping(path = Array("/schedule"),  produces=Array(MediaType.APPLICATION_JSON_VALUE))
-  def listAll(): JsonObject =  {
+  @GetMapping(path = Array("/schedule"))
+  def listAll(): String =  {
     val wp = new WeatherDataProcessing()
-    val de = wp.toJsonString
-    val data = de.toString
+    val data = wp.toJsonString.toString
 
     val filename = s"${LocalDateTime.now().withNano(0)}.json"
     Files.createFile(Paths.get(filename))
     Files.write(Paths.get(filename), data.getBytes())
     CloudStorage.upload(Paths.get(filename))
     Files.delete(Paths.get(filename))
-    de
+    data
   }
 }
